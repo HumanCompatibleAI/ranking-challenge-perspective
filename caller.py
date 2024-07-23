bdc052b18c0998dfd01e7da403861126bcbb1878
Ian Baker <ian@sonic.net>
2024-07-22 22:57:29 -0700

    fix caller.py

--- caller.py
+++ caller.py
@@ -1,17 +1,23 @@
 import time
 from pprint import pprint
 
+from fastapi.encoders import jsonable_encoder
+
 import requests
-import sample_data
+from ranking_challenge.fake import fake_request
 
 # This is a simple script that sends a POST request to the API and prints the response.
 # Your server should be running on localhost:8000
 
-# Wait for the Flask app to start up
+# Wait for the app to start up
 time.sleep(2)
 
+# make some fake request data
+items = fake_request(n_posts=1, n_comments=2)
+items.session.cohort = "arm1"
+
 # Send POST request to the API
-response = requests.post("http://localhost:8000/rank", json=sample_data.comments)
+response = requests.post("http://localhost:8000/rank", json=jsonable_encoder(items))
 
 # Check if the request was successful (status code 200)
 if response.status_code == 200:
